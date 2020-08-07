bc747fb9a75e10c7b25d06f7746eb59ae4879771
ZastrixArundell <zastrixarundell@gmail.com>
2020-08-07 20:51:16 +0200

    Disabled dye image timer

--- src/main/java/com/github/zastrixarundell/toramsensei/ToramSensei.java
+++ src/main/java/com/github/zastrixarundell/toramsensei/ToramSensei.java
@@ -78,7 +78,7 @@ public class ToramSensei
         Scanner scanner = new Scanner(System.in);
 
         Timer activity = updateActivity(bot);
-        Timer dyeImage = updateDyesImage(bot);
+        // Timer dyeImage = updateDyesImage(bot);
         Timer updates = updateUpdates(bot);
 
         try

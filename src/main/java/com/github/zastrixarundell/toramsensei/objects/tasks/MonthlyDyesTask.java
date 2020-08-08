c77e70b9de1125c25e40fee0eae58147d4823e4d
ZastrixArundell <zastrixarundell@gmail.com>
2020-08-08 21:28:55 +0200

    Checked hash DB IO works

--- src/main/java/com/github/zastrixarundell/toramsensei/objects/tasks/MonthlyDyesTask.java
+++ src/main/java/com/github/zastrixarundell/toramsensei/objects/tasks/MonthlyDyesTask.java
@@ -1,7 +1,11 @@
 package com.github.zastrixarundell.toramsensei.objects.tasks;
 
+import com.github.zastrixarundell.toramsensei.Database;
+import com.github.zastrixarundell.toramsensei.Helpers;
 import org.javacord.api.DiscordApi;
 
+import java.sql.Connection;
+import java.util.Optional;
 import java.util.TimerTask;
 
 public class MonthlyDyesTask extends TimerTask
@@ -14,5 +18,21 @@ public class MonthlyDyesTask extends TimerTask
     @Override
     public void run()
     {
+        try
+        {
+            Connection connection = Database.getConnection();
+
+            Database.createHashTable(connection);
+
+            Optional<String> cachedHash = Database.getCachedHash(connection);
+
+            Helpers.MonthlyHashObject monthlyImage = Helpers.getMonthlyImage();
+
+            Database.setHashElement(connection, "AAAAAA");
+        }
+        catch (Exception e)
+        {
+
+        }
     }
 }
